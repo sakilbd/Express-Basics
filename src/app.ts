87286819b1f853c6f05c10967ab2b056b649e3bf
sakilbd <mdsakiluzzaman@gmail.com>
2023-11-15 17:49:27 +0600

    first push

---
+++ src/app.ts
@@ -0,0 +1,98 @@
+
+import express, { NextFunction, Request, Response } from 'express';
+// const express = require('express')
+const app = express()
+const port = 3000
+
+//parsers 
+app.use(express.json());
+app.use(express.text());
+
+//router
+
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.get("/create-user", (req: Request, res: Response) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    })
+})
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Course is created successfully",
+        data: course,
+    })
+})
+
+
+//middleware 
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+}
+// app.get('/:userId/:subId', (req: Request, res: Response) => {
+//     console.log(req.params);
+//     res.send('Hello war!')
+// })
+
+app.get('/', logger, async (req: Request, res: Response, next: NextFunction) => {
+    // console.log(req.query); //logs all query 
+    // console.log(req.query.email);  //returns after ?email=sakil@gmail.com
+    try {
+        res.send("something");
+    } catch (err) {
+        console.log(err);
+        next(err);
+
+    }
+
+    res.send('Hello war!')
+})
+
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body);
+    res.json({
+        message: "successfully received data"
+    });
+})
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
+
+
+/// used for worng route info but didnt work check the video again 
+app.all("*", (req: Request, res: Response) => {
+    res.status(400).json({
+        success: false,
+        message: "Router Not found"
+    })
+})
+
+
+
+//global error handler 
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "failed to get data"
+        })
+    }
+
+})
+
+export default app;
